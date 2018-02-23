3c6da1307d8e090b80f0ed9ba8fd03cab18d495e
berli <libo22@gmail.com>
2018-02-23 18:34:34 +0800

    modified:   index.html 	modified:   proses.php

--- proses.php
+++ proses.php
@@ -15,17 +15,15 @@
 	include 'IPTC.php';
 	$target_dir = "uploads/";
 	$file_name_before = basename($_FILES["takePictureFieldBefore"]["name"]);
-	$file_name_after = basename($_FILES["takePictureFieldAfter"]["name"]);
 
 	$target_file_before = $target_dir . $file_name_before;
-	$target_file_after = $target_dir . $file_name_after;
 
 	$uploadOk = 1;
 	$imageFileType_before = pathinfo($target_file_before, PATHINFO_EXTENSION);
-	$imageFileType_after = pathinfo($target_file_after, PATHINFO_EXTENSION);
 
 	// Check if image file is a actual image or fake image
-	if(isset($_POST)) {
+	if(isset($_POST)) 
+	{
 		// check existance upload target directory
 		if( !is_dir($target_dir) ) @mkdir($target_dir);
 
@@ -33,23 +31,14 @@
 	    if($check_before !== false) 
 	    { 
 	        $uploadOk = 1;
-	        if (file_exists($target_file_before)) {
+			if (file_exists($target_file_before)) 
+			{
 			    unlink($target_file_before);
 			}
-	    } else {
+		} 
+		else 
+		{
 	        echo "File Before is not an image.";
-	        $uploadOk = 0;
-	    }
-
-	    $check_after = getimagesize($_FILES["takePictureFieldAfter"]["tmp_name"]);
-	    if($check_after !== false) 
-	    { 
-	        $uploadOk = 1;
-	        if (file_exists($target_file_after)) {
-			    unlink($target_file_after);
-			}
-	    } else {
-	        echo "File After is not an image.";
 	        $uploadOk = 0;
 	    }
 
@@ -58,9 +47,8 @@
 		    echo "Sorry, your file was not uploaded.";
 		} else {
 			$do_upload_before = move_uploaded_file($_FILES["takePictureFieldBefore"]["tmp_name"], $target_file_before);
-			$do_upload_after = move_uploaded_file($_FILES["takePictureFieldAfter"]["tmp_name"], $target_file_after);
 
-		    if ($do_upload_before && $do_upload_after)
+		    if ($do_upload_before )
 		    {
 		    	// write final EXIF TAG
 		    	$objIPTC_before = new IPTC($target_file_before);
@@ -71,19 +59,8 @@
 		    	$objIPTC_before->setValue(IPTC_ORIGINATING_PROGRAM, "originating apps");
 		    	$objIPTC_before->setValue(IPTC_SOURCE, "IPTC_SOURCE");
 
-		    	$objIPTC_after = new IPTC($target_file_after);
-		    	$objIPTC_after->setValue(IPTC_COPYRIGHT_STRING, "A copyright notice");
-		    	$objIPTC_after->setValue(IPTC_CAPTION, "A caption descriptions for this picture [AFTER]."); 
-		    	$objIPTC_after->setValue(IPTC_FIXTURE_IDENTIFIER, "fixture identifier");
-		    	$objIPTC_after->setValue(IPTC_CREDIT, "IPTC_CREDIT");
-		    	$objIPTC_after->setValue(IPTC_ORIGINATING_PROGRAM, "originating apps");
-		    	$objIPTC_after->setValue(IPTC_SOURCE, "IPTC_SOURCE");
-
 		        echo "The file ". basename( $file_name_before ) . " has been uploaded. <br/>";
 		        echo "<img src='" . $target_file_before . "' /><br/>";
-
-		        echo "The file ". basename( $file_name_after ). " has been uploaded. <br/>";
-		        echo "<img src='" . $target_file_after . "' /><br/>";
 
 		    } else {
 		        echo "Sorry, there was an error uploading your file.";
